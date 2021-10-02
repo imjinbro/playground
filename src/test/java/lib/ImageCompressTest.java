af83964bef15beb0a8dcce8beef9f2122fe58669
imjinbro <jinbro@likelion.org>
2021-10-02 14:12:55 +0900

    image s3 upload, image compress

---
+++ src/test/java/lib/ImageCompressTest.java
@@ -0,0 +1,34 @@
+package lib;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.FileImageOutputStream;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+
+class ImageCompressTest {
+
+	@Test
+	void dyddpccet() throws Exception {
+		File file = new ClassPathResource("/image/test-image.jpg").getFile();
+		BufferedImage image = ImageIO.read(file);
+
+		ImageWriter writer = ImageIO.getImageWritersByFormatName("jpeg").next();
+		ImageWriteParam param = writer.getDefaultWriteParam();
+		param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+		param.setCompressionQuality(0.5f);
+
+		Path path = Paths.get(this.getClass().getResource("/image").getPath());
+		File resizedImageFile = new File(path.toString(), "resized.jpg");
+		FileImageOutputStream output = new FileImageOutputStream(resizedImageFile);
+		writer.setOutput(output);
+		IIOImage iioImage = new IIOImage(image, null, null);
+		writer.write(null, iioImage, param);
+	}
+}
---
+++ src/test/java/lib/S3ImageUpload.java
@@ -0,0 +1,61 @@
+package lib;
+
+import static org.testcontainers.containers.localstack.LocalStackContainer.Service.S3;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import java.io.File;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import org.junit.Rule;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.utility.DockerImageName;
+
+/*
+	AWS S3에 직접 업로드 하지 않고, docker localstack 컨테이너로 올린 s3에 업로드
+ */
+class S3ImageUpload {
+
+	@Rule
+	private LocalStackContainer localstack = new LocalStackContainer(DockerImageName.parse("localstack/localstack:0.11.3")).withServices(S3);
+
+	@BeforeEach
+	void setUp() {
+		localstack.start();
+	}
+
+	@Test
+	@DisplayName("S3 이미지 업로드")
+	void fqxphstyp() throws Exception {
+		AmazonS3 s3Client = AmazonS3ClientBuilder
+			.standard()
+			.withEndpointConfiguration(localstack.getEndpointConfiguration(S3))
+			.withCredentials(localstack.getDefaultCredentialsProvider())
+			.build();
+
+		File imageFile = new ClassPathResource("/image/test-image.jpg").getFile();
+		String bucketName = "test-image";
+		String dirName = "upload";
+		String fileName = dirName + "/" + imageFile.getName();
+
+		ObjectMetadata metadata = new ObjectMetadata();
+		Date expireTime = Date.from(LocalDateTime.now().plusDays(1L).atZone(ZoneId.systemDefault()).toInstant());
+		metadata.setExpirationTime(expireTime);
+
+		s3Client.createBucket(bucketName);
+		s3Client.putObject(new PutObjectRequest(bucketName, fileName, imageFile).withMetadata(metadata));
+
+		String imageUri = s3Client.getUrl(bucketName, fileName).toURI().toString();
+		System.out.println(imageUri);
+
+		metadata = s3Client.getObjectMetadata(bucketName, fileName);
+		System.out.println(metadata);
+	}
+}
